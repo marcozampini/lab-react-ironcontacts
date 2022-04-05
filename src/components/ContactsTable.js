1964e17ec55bf77312e2f4f31cb0553b55f8ecfa
Marco Zampini <marco@bloom-it.io>
2022-04-05 17:24:38 +0200

    Iteration 4

--- src/components/ContactsTable.js
+++ src/components/ContactsTable.js
@@ -19,11 +19,23 @@ const ContactsTable = (props) => {
     setContactsList(newContactList)
     setRemainingContacts(newRemainingContactList)
   }
+  const sortByPopularity = () => {
+    const newContactList = [...contactsList]
+    newContactList.sort((a, b) => b.popularity - a.popularity)
+    setContactsList(newContactList)
+  }
+  const sortByName = () => {
+    const newContactList = [...contactsList]
+    newContactList.sort((a, b) => a.name.localeCompare(b.name))
+    setContactsList(newContactList)
+  }
 
   return (
     <>
       <h1>IronContacts</h1>
       <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+      <button onClick={sortByName}>Sort by name</button>
       <table>
         <thead>
           <tr>
